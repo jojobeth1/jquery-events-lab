f58a685ff249597205a3591935fd4ed98e2bc542
jojobeth1@gmail.com <jojobeth1@gmail.com>
2018-01-16 15:43:23 -0800

    ready js

--- starter-code/ready/base.js
+++ starter-code/ready/base.js
@@ -3,7 +3,7 @@
 */
 
 $(document).ready(function(){
-  console.log( "The page says:", $("h1").text() );
+  console.log( "The page says:", $("h1").text("Go!") );
 });
 
 $("h1").text("Go!");
@@ -14,3 +14,15 @@ if ( $("h1").text() === "Go!" ){
 } else {
   console.log( 'Can you make it say "Go!"?' );
 }
+
+
+$(document).ready(function(){
+  $("h1").text("Go!");
+
+  if ( $("h1").text() === "Go!" ){
+    console.log( "The page says:", $("h1").text() );
+    console.log("Success!");
+  } else {
+    console.log( 'Can you make it say "Go!"?' );
+  }
+});
